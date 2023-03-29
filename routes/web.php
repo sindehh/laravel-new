85a312077a5d76683c149b83b5e17d0bcf205b10
sindehh <113077151+sindehh@users.noreply.github.com>
2023-03-29 16:35:00 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -14,13 +14,19 @@ use App\Http\Controllers\UserController;
 |
 */
 
-Route::get('/', [CustomerController::class, 'index']);
-Route::get('/register', [UserController::class, 'register']);
+Route::get('/', [CustomerController::class, 'index'])->middleware('auth');
+Route::get('/addCustomer',[CustomerController::class, 'addCustomer'])->middleware('auth');
+Route::post('/saveCustomer', [CustomerController::class, 'saveCustomer']);
+Route::get('/edit/{id}', [CustomerController::class, 'edit'])->middleware('auth');
+Route::post('updateCustomer', [CustomerController::class, 'updateCustomer'])->middleware('auth');
+
+Route::get('/user', [UserController::class, 'useer'])->middleware('auth');
 Route::get('/login', [UserController::class, 'login'])->name('login')->middleware('guest');
+Route::get('/register', [UserController::class, 'register']);
+
+
 
 Route::post('/login/process', [UserController::class, 'process']);
 Route::post('/store', [UserController::class, 'store']);
-
 Route::get('/logout', [UserController::class, 'logout']);
-
-Route::get('delete/{id}', [CustomerController::class, 'delete'])->middleware('auth');
+Route::get('/delete/{id}', [UserController::class, 'delete'])->middleware('auth');
